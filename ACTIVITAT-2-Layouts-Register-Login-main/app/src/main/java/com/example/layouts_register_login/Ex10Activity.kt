adde7f332c55b277b92ab48cab076f19acb8f2a3
Ashley <a24ashcasboi@inspedralbes.cat>
2026-03-05 20:40:51 +0100

    todos los ejercicios

---
+++ ACTIVITAT-2-Layouts-Register-Login-main/app/src/main/java/com/example/layouts_register_login/Ex10Activity.kt
@@ -0,0 +1,24 @@
+package com.example.layouts_register_login
+
+import com.example.layouts_register_login.R
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class Ex10Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ex10)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        findViewById<Button>(R.id.btnHorizontal).setOnClickListener {
+            startActivity(Intent(this, Ex10HorizontalActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.btnVertical).setOnClickListener {
+            startActivity(Intent(this, Ex10VerticalActivity::class.java))
+        }
+    }
+}
+
